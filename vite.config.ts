5327d52c6b1cd67206af0164c6a4bd1273532e82
Dileep <dileep@Dileeps-MacBook-Air.local>
2025-08-13 11:27:24 +0100

    allowedhost

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["localhost", "prima-health-application.onrender.com"],
   },
   plugins: [
     react(),
